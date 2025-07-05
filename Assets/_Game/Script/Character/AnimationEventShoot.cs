abda6e41ba83337e98695c1cdcbe6ac8f5883e0c
HiepTran <hiep61027@gmail.com>
2025-07-05 16:00:40 +0700

    Fix map

---
+++ Assets/_Game/Script/Character/AnimationEventShoot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class AnimationEventShoot : MonoBehaviour
+{
+    [SerializeField] private Character Character;
+
+    public void CallThrow()
+    {
+        Debug.Log("Shooted");
+        Character.Throw();
+    }
+}
+
